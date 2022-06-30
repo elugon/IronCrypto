f42068a050f8706e990f48db8bc1c50d46c548c7
Alberto <aquintero88@gmail.com>
2022-06-30 18:47:24 +0200

    login in progress

--- routes/favorites.js
+++ routes/favorites.js
@@ -14,10 +14,10 @@ router.get('/', isLoggedIn, async (req, res, next) => {
         const user = await User.findOne({ name: username });
         // let btc = [967.6,957.02,190.78];
         // sparkline.sparkline(document.querySelector(".btc"), btc);
-        //const data = await CoinGeckoClient.coins.fetch('bitcoin', {tickers: false, community_data: false, developer_data:false, localization:false, sparkline: true}); 
-        //const retrievePriceUsd = data
+        const data = await CoinGeckoClient.coins.all(); 
+        const retrievePriceUsd = data
         //res.json(retrievePriceUsd)
-        res.render('favorites', {user} );        
+        res.render('favorites',{user,data} );        
     } catch (error) {
         next(error)
         
